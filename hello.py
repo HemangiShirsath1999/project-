9a4cce2799ba49c6ba1e142ca997e84bea03f54f
HemangiShirsath1999 <nikamhemangi1999@gmail.com>
2022-03-19 16:35:53 +0530

    add program

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+print("hello")
+a = "harry"
+b = 345
+c = 45.32
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+a = 3
+b = 4
+print("the value 3+4 is",3+4)
+print("the value 3-4 is",3-4)
