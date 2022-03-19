aa1b0fa794edf526a3d67a6e31b2b9c6ca356e64
HemangiShirsath1999 <nikamhemangi1999@gmail.com>
2022-03-19 16:55:38 +0530

    add program

---
+++ video6.py
@@ -0,0 +1,8 @@
+#single line comments
+print("subscribe hemangi now")
+"""multi line comments
+hemangi
+imrd cllege"""
+print("hello hemanhi","shirsath")
+print("hemangi",end=" ")
+print("hemangi \n shirsath \t")
